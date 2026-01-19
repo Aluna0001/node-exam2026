4e7b07033f6b76d7399157ee2f5d23ef50883a7e
Aluna0001 <lusz0001@stud.ek.dk>
2026-01-19 20:27:37 +0100

    Added new index.html file

--- client/src/components/LoginForm.svelte
+++ client/src/components/LoginForm.svelte
@@ -1,29 +1,29 @@
 <script>
-    import { login } from '../services/authService'
+  import { login } from '../services/authService'
 
-    let username = $state('')
-    let password = $state('')
+  let username = $state('')
+  let password = $state('')
 
-    async function handleLogin() {
-        console.log('Login attempt')
-        const result = await login(username, password)
+  async function handleLogin(event) {
+    event.preventDefault()
+    console.log('Login attempt')
+    const result = await login(username, password)
 
-        if (result.error) {
-            console.error('Login failed:', result.error)
-        } else {
-            console.log('Login success', result)
-        }
+    if (result.error) {
+      console.error('Login failed:', result.error)
+    } else {
+      console.log('Login success', result)
     }
-    
+  }
 </script>
 
 <h1>Login</h1>
 
-<input type="text" placeholder="Username" bind:value={username}/>
-<input type="password" placeholder="Password" bind:value={password}/>
-
-<button onclick={handleLogin}>Login</button>
-<p>Du skrev: {username}</p>
+<form onsubmit={handleLogin}>
+  <input type="text" placeholder="Username" bind:value={username} required />
+  <input type="password" placeholder="Password" bind:value={password} required />
+  <button type="submit">Login</button>
+</form>
 
 <style>
 
