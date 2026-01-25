4187251c2bc31a253ecdc1e5cdaa2e154f8d7e4c
Aluna0001 <lusz0001@stud.ek.dk>
2026-01-25 17:07:10 +0100

    Added socket.io setup for Websocket functionality

--- server/app.js
+++ server/app.js
@@ -1,47 +1,66 @@
-import 'dotenv/config'
-import express from 'express'
-import helmet from 'helmet'
-import cors from 'cors'
-import { sessionMiddleware } from './middleware/sessionConfig.js'
-import { generalLimiter, authLimiter } from './middleware/rateLimiters.js'
+import "dotenv/config";
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { sessionMiddleware } from "./middleware/sessionConfig.js";
+import { generalLimiter, authLimiter } from "./middleware/rateLimiters.js";
 
-import authRoutes from './routes/authRoutes.js'
-import tarotRoutes from './routes/tarotRoutes.js'
-import signupRoutes from './routes/signupRoutes.js'
+import authRoutes from "./routes/authRoutes.js";
+import tarotRoutes from "./routes/tarotRoutes.js";
+import signupRoutes from "./routes/signupRoutes.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  })
-)
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
-app.use(helmet())
+app.use(helmet());
 
-app.use(sessionMiddleware)
+app.use(sessionMiddleware);
 
-app.use(generalLimiter)
+app.use(generalLimiter);
 
-// Static files after security, but before routes
-app.use('/images', express.static('./public/images'))
+// Static files after security, but before route
+app.use("/images", express.static("./public/images"));
 
-app.use('/auth', authLimiter)
-app.use(authRoutes)
-app.use(tarotRoutes)
-app.use(signupRoutes)
+app.use("/auth", authLimiter);
+app.use(authRoutes);
+app.use(tarotRoutes);
+app.use(signupRoutes);
 
-app.use(express.static('./../client/dist'))
+app.use(express.static("./../client/dist"));
 
-app.all('/{*splat}', (req, res) => {
-  res.send({ data: 'Nothing to see here' })
-})
+app.all("/{*splat}", (req, res) => {
+  res.send({ data: "Nothing to see here" });
+});
 
-const PORT = Number(process.env.PORT) || 8080
+const PORT = Number(process.env.PORT) || 8080;
 
-app.listen(PORT, () => {
-  console.log('Server is running on port:', PORT)
-})
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log("Server is running on port:", PORT);
+});
