9944951e19cf6c53a676266087eca96d9f786d33
Aluna0001 <lusz0001@stud.ek.dk>
2026-01-31 16:42:52 +0100

    Fix chat username bug by disconnecting socket before reconnecting with new user

--- client/src/services/socketService.js
+++ client/src/services/socketService.js
@@ -1,24 +1,28 @@
-import { io } from 'socket.io-client'
-import { get } from 'svelte/store'
-import { account } from '../stores/auth.js'
+import { io } from "socket.io-client";
+import { get } from "svelte/store";
+import { account } from "../stores/auth.js";
 
-const socket = io('http://localhost:8080', {
-  autoConnect: false
-})
+const socket = io("http://localhost:8080", {
+  autoConnect: false,
+});
 
-export function connectSocket () {
-  const user = get(account)
+export function connectSocket() {
+  const user = get(account);
 
-  socket.auth = {
-    username: user?.username
+  if (socket.connected) {
+    socket.disconnect();
   }
 
-  socket.connect()
-  console.log('Socket connecting as:', user?.username)
+  socket.auth = {
+    username: user?.username,
+  };
+
+  socket.connect();
+  console.log("Socket connecting as:", user?.username);
 }
 
-export function disconnectSocket () {
-  socket.disconnect()
+export function disconnectSocket() {
+  socket.disconnect();
 }
 
-export default socket
+export default socket;
